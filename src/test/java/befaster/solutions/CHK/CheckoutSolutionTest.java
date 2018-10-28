797d38bdc827607159809e86b591867e0fb002e6
sbx_user1060 <sbx_user1060@ip-10-128-125-177.eu-west-2.compute.internal>
2018-10-28 17:10:53 +0000

    Sun Oct 28 17:10:53 UTC 2018

--- src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
+++ src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
@@ -1,4 +1,35 @@
 package befaster.solutions.CHK;
 
+import org.junit.Before;
+import org.junit.Test;
+
+
+/*
+Our price table and offers:
++------+-------+----------------+
+| Item | Price | Special offers |
++------+-------+----------------+
+| A    | 50    | 3A for 130     |
+| B    | 30    | 2B for 45      |
+| C    | 20    |                |
+| D    | 15    |                |
++------+-------+----------------+
+ */
+
 public class CheckoutSolutionTest {
+
+    private CheckoutSolution checkoutSolution;
+
+    @Before
+    public void setup() {
+        checkoutSolution = new CheckoutSolution();
+    }
+
+    @Test
+    public void test() {
+
+
+    }
+
+
 }
