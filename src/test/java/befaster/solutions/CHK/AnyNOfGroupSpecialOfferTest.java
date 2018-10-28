df4991b41c76a14bc46d89954e4d06930bc2d9e1
sbx_user1059 <sbx_user1059@ip-10-128-83-247.eu-west-2.compute.internal>
2018-10-28 23:46:03 +0000

    Sun Oct 28 23:46:03 UTC 2018

--- src/test/java/befaster/solutions/CHK/AnyNOfGroupSpecialOfferTest.java
+++ src/test/java/befaster/solutions/CHK/AnyNOfGroupSpecialOfferTest.java
@@ -16,13 +16,21 @@ public class AnyNOfGroupSpecialOfferTest {
         List<Item> shoppingBasket1 = Arrays.asList(T, X, Y, Z);
         List<Item> shoppingBasket2 = Arrays.asList(S, X, Y, Z);
         List<Item> shoppingBasket3 = Arrays.asList(Z, Z, X, X, X);
+        List<Item> shoppingBasket4 = Arrays.asList(T, X);
         SpecialOffer specialOffer = new AnyNOfGroupSpecialOffer(3, Arrays.asList(S, T, X, Y, Z),45 );
 
 
+        assertThat(specialOffer.doesApply(shoppingBasket1)).isTrue();
         assertThat(specialOffer.apply(shoppingBasket1)).containsOnlyOnce(X);
 
+        assertThat(specialOffer.doesApply(shoppingBasket2)).isTrue();
         assertThat(specialOffer.apply(shoppingBasket2)).containsOnlyOnce(X);
+
+        assertThat(specialOffer.doesApply(shoppingBasket3)).isTrue();
         assertThat(specialOffer.apply(shoppingBasket3)).containsExactlyInAnyOrder(X, X);
+
+
+        assertThat(specialOffer.doesApply(shoppingBasket4)).isFalse();
 
     }
 
