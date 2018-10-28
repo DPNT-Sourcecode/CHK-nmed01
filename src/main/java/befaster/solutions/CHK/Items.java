0837ec9924273759c7ee7cc558e20b8eaccca510
sbx_user1059 <sbx_user1059@ip-10-128-83-247.eu-west-2.compute.internal>
2018-10-28 22:23:25 +0000

    Sun Oct 28 22:23:25 UTC 2018

--- src/main/java/befaster/solutions/CHK/Items.java
+++ src/main/java/befaster/solutions/CHK/Items.java
@@ -1,4 +1,65 @@
 package befaster.solutions.CHK;
 
-public enum Items {
+/*
++------+-------+------------------------+
+| Item | Price | Special offers         |
++------+-------+------------------------+
+| A    | 50    | 3A for 130, 5A for 200 |
+| B    | 30    | 2B for 45              |
+| C    | 20    |                        |
+| D    | 15    |                        |
+| E    | 40    | 2E get one B free      |
+| F    | 10    | 2F get one F free      |
+| G    | 20    |                        |
+| H    | 10    | 5H for 45, 10H for 80  |
+| I    | 35    |                        |
+| J    | 60    |                        |
+| K    | 80    | 2K for 150             |
+| L    | 90    |                        |
+| M    | 15    |                        |
+| N    | 40    | 3N get one M free      |
+| O    | 10    |                        |
+| P    | 50    | 5P for 200             |
+| Q    | 30    | 3Q for 80              |
+| R    | 50    | 3R get one Q free      |
+| S    | 30    |                        |
+| T    | 20    |                        |
+| U    | 40    | 3U get one U free      |
+| V    | 50    | 2V for 90, 3V for 130  |
+| W    | 20    |                        |
+| X    | 90    |                        |
+| Y    | 10    |                        |
+| Z    | 50    |                        |
++------+-------+------------------------+
+
+ */
+public class Items {
+
+    public static Item A = new Item('A', 50);
+    public static Item B = new Item('B', 50);
+    public static Item C = new Item('C', 50);
+    public static Item D = new Item('D', 50);
+    public static Item E = new Item('E', 50);
+    public static Item F = new Item('F', 50);
+    public static Item G = new Item('G', 50);
+    public static Item H = new Item('H', 50);
+    public static Item I = new Item('I', 50);
+    public static Item J = new Item('J', 50);
+    public static Item K = new Item('K', 50);
+    public static Item L = new Item('L', 50);
+    public static Item M = new Item('M', 50);
+    public static Item N = new Item('N', 50);
+    public static Item O = new Item('O', 50);
+    public static Item P = new Item('P', 50);
+    public static Item Q = new Item('Q', 50);
+    public static Item R = new Item('R', 50);
+    public static Item S = new Item('S', 50);
+    public static Item T = new Item('T', 50);
+    public static Item U = new Item('U', 50);
+    public static Item V = new Item('V', 50);
+    public static Item W = new Item('W', 50);
+    public static Item X = new Item('X', 50);
+    public static Item Y = new Item('Y', 50);
+    public static Item Z = new Item('Z', 50);
+
 }
