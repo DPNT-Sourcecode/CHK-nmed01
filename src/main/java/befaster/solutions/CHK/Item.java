6469e93ebb2c8b88556f896099f57991514063fd
sbx_user1060 <sbx_user1060@ip-10-128-125-177.eu-west-2.compute.internal>
2018-10-28 17:13:53 +0000

    Sun Oct 28 17:13:53 UTC 2018

--- src/main/java/befaster/solutions/CHK/Item.java
+++ src/main/java/befaster/solutions/CHK/Item.java
@@ -1,4 +1,21 @@
 package befaster.solutions.CHK;
 
 public class Item {
+
+    private final char code;
+    private final int price;
+
+
+    public Item(char code, int price) {
+        this.code = code;
+        this.price = price;
+    }
+
+    public char getCode() {
+        return code;
+    }
+
+    public int getPrice() {
+        return price;
+    }
 }
--- src/main/java/befaster/solutions/CHK/SpecialOffer.java
+++ src/main/java/befaster/solutions/CHK/SpecialOffer.java
@@ -1,4 +1,26 @@
 package befaster.solutions.CHK;
 
 public class SpecialOffer {
+
+    private final int quantity;
+    private final Item item;
+    private final int promotionPrice;
+
+    public SpecialOffer(int quantity, Item item, int promotionPrice) {
+        this.quantity = quantity;
+        this.item = item;
+        this.promotionPrice = promotionPrice;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public int getPromotionPrice() {
+        return promotionPrice;
+    }
 }
