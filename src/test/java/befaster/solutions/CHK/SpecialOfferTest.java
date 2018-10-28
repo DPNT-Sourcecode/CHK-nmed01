335d657d2431dbada6b626cd574258ac63c813e6
sbx_user1060 <sbx_user1060@ip-10-128-125-177.eu-west-2.compute.internal>
2018-10-28 17:49:53 +0000

    Sun Oct 28 17:49:53 UTC 2018

--- src/test/java/befaster/solutions/CHK/SpecialOfferTest.java
+++ src/test/java/befaster/solutions/CHK/SpecialOfferTest.java
@@ -11,7 +11,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class SpecialOfferTest {
 
     @Test
-    public void promotionApplyTest() {
+    public void doesApplyTest() {
 
         SpecialOffer specialOffer = new SpecialOffer(Arrays.asList(
                 new Item('A', 60),
@@ -33,5 +33,47 @@ public class SpecialOfferTest {
         assertThat(specialOffer.doesApply(shoppingBasket2)).isFalse();
 
     }
+
+    @Test
+    public void applyTest() {
+
+        SpecialOffer specialOffer = new SpecialOffer(Arrays.asList(
+                new Item('A', 60),
+                new Item('A', 60)
+        ), 100);
+
+        List<Item> shoppingBasket1 = Arrays.asList(
+                new Item('A', 60),
+                new Item('A', 60),
+                new Item('B', 20)
+        );
+
+        List<Item> shoppingBasketAfterPromotion = Arrays.asList(
+                new Item('B', 20)
+        );
+
+
+        assertThat(specialOffer.apply(shoppingBasket1)).hasSameElementsAs(shoppingBasketAfterPromotion);
+
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void applyOnNotApplicableShoppingBasketTest() {
+
+        SpecialOffer specialOffer = new SpecialOffer(Arrays.asList(
+                new Item('A', 60),
+                new Item('A', 60)
+        ), 100);
+
+        List<Item> shoppingBasket2 = Arrays.asList(
+                new Item('A', 60),
+                new Item('B', 20)
+        );
+
+        specialOffer.apply(shoppingBasket2);
+
+
+    }
+
 
 }
