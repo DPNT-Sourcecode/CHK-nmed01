ff152cfd267ed7972242f9b0cef3e0a3228bf304
sbx_user1060 <sbx_user1060@ip-10-128-125-177.eu-west-2.compute.internal>
2018-10-28 17:43:53 +0000

    Sun Oct 28 17:43:53 UTC 2018

--- src/test/java/befaster/solutions/CHK/SpecialOfferTest.java
+++ src/test/java/befaster/solutions/CHK/SpecialOfferTest.java
@@ -1,7 +1,32 @@
 package befaster.solutions.CHK;
 
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class SpecialOfferTest {
 
-    
+    @Test
+    public void promotionApplyTest() {
+
+        SpecialOffer specialOffer = new SpecialOffer(Arrays.asList(
+                new Item('A', 60),
+                new Item('A', 60)
+        ), 100);
+
+        List<Item> shoppingBasket = Arrays.asList(
+                new Item('A', 60),
+                new Item('A', 60),
+                new Item('B', 20)
+        );
+
+
+        assertThat(specialOffer.doesApply(shoppingBasket), isTrue())
+
+
+    }
 
 }
